9789676495244eb996d90f604140d21b91a8b8e7
gandalf458 <gandalf458@gmail.com>
2016-10-04 17:15:32 +0200

    1st commit
    Added script, license and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# List-doc-titles-and-descriptions
+
+This is a simple (and possibly incomplete) PHP script to list the document titles and meta descriptions for a set of web pages (usually a complete website). Its aim is to help with website SEO by giving a visual view of the title and description of all indexable HTML documents in a website together with the length of each.
+
+The script is a single file named listmeta.php which contains the necessary HTML, PHP and CSS code.
+
+The script requires a Yahoo!-style urllist.txt file listing the URLs to be processed. Many sitemap generators will create a urllist.txt file as well as a Google sitemap.xml document.
---
+++ listmeta.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en-GB">
+<head>
+<meta charset="utf-8">
+<title>List document titles and descriptions</title>
+<?php
+/*
+ * PHP script to list document title and description for a list of HTML pages
+ * (c) Copyright 2016, Irwin Associates and Graham R Irwin - www.irwinassociates.eu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+?>
+<style>
+body {
+  font: .83rem Verdana, Arial, Helvetica, sans-serif;
+}
+h1 {
+  font-size: 160%;
+}
+table {
+  border-collapse: collapse;
+}
+tr:nth-child(odd) {
+  background-color: #eef;
+}
+tr:first-child {
+  background-color: #dde;
+}
+th, td {
+  padding: 3px;
+  border: 1px solid #ccc;
+}
+</style>
+</head>
+<body>
+<h1>List of document titles and descriptions</h1>
+<table>
+  <tr>
+    <th scope="col">Document</th>
+    <th scope="col">Title 65,69,72</th>
+    <th scope="col">Description 150,156,161</th>
+  </tr>
+  <?php
+  // Turn off all error reporting; comment out if you want errors reported
+  error_reporting(0);
+
+  // read list of files into array
+  $flist = 'urllist.txt';
+  if ( !file_exists($flist) )
+    die('file '.$flist.' does not exist!');
+  $files = file($flist);
+
+  $ignore = array('pdf', 'txt');
+  foreach ( $files as $file ) {
+    $file = trim($file);
+
+    // check file type
+    $extn = substr(strrchr($file, '.'), 1);
+    if ( in_array($extn, $ignore) )
+      continue;
+
+    // get html
+    $doc = new DOMDocument();
+    $doc->loadHTMLFile($file);
+
+    // get document title
+    $nodes = $doc->getElementsByTagName('title');
+    $title = $nodes->item(0)->textContent;
+    $t_len = strlen($title);
+
+    // get meta description
+    $descr = '';
+    $metas = $doc->getElementsByTagName('meta');
+    foreach ( $metas as $meta ) {
+      $name = $meta->getAttribute("name");
+      if ( $name == 'description' )
+        $descr = $meta->getAttribute("content");
+    }
+    $d_len = strlen($descr);
+
+    // output result
+    echo '  <tr>', "\n";
+    echo '    <td>', substr(strrchr($file, '/'), 1), '</td>', "\n";
+    echo '    <td>', $title, ' (', $t_len, ')</td>', "\n";
+    echo '    <td>', $descr, ' (', $d_len, ')</td>', "\n";
+    echo '  </tr>', "\n";
+  }
+  ?>
+</table>
+</body>
+</html>
